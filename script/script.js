f173e30feef1164dced23cb6d125dc992bb853c3
Marcos Fernandes <contato@marcos-fernandes.com>
2019-02-15 19:26:26 -0200

    form update

--- script/script.js
+++ script/script.js
@@ -1,8 +1,14 @@
 window.onscroll = function() {myFunction()};
 
 var navbar = document.getElementById("navbar");
-
 var sticky = navbar.offsetTop;
+var errorMsg = document.getElementById('errorMsg');
+
+const nameField = document.getElementById('nameField');
+const resetButton = document.getElementById('reset-button');
+
+
+resetButton.addEventListener('click', clearErrorMsg);
 
 function myFunction() {
   if (window.pageYOffset >= sticky) {
@@ -10,4 +16,22 @@ function myFunction() {
   } else {
     navbar.classList.remove("sticky");
   }
+}
+
+function validateForm() {
+    var nameCheck = document.forms["contatoform"]["name"].value;
+    var mailCheck = document.forms["contatoform"]["mail"].value;
+    var commentCheck = document.forms["contatoform"]["comment"].value;
+    if (nameCheck == "" || mailCheck == "" || commentCheck == "") {
+      errorMsg.textContent = "Por favor, preencha todos os campos.";
+      return false;
+    } else {
+        errorMsg.textContent = "";
+        return true;
+    }
+  }
+
+function clearErrorMsg() {
+    errorMsg.textContent = "";
+    nameField.focus();
 }
