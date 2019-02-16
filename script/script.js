f7df8c644720d6c2f35b114343ea4bc6570c292c
Marcos Fernandes <contato@marcos-fernandes.com>
2019-02-16 11:12:20 -0200

    campo de contato alterado. Botao para retornar ao topo adicionado.

--- script/script.js
+++ script/script.js
@@ -1,37 +1,40 @@
-window.onscroll = function() {myFunction()};
+window.onscroll = function() {scrollFunction()};
 
 var navbar = document.getElementById("navbar");
 var sticky = navbar.offsetTop;
-var errorMsg = document.getElementById('errorMsg');
 
-const nameField = document.getElementById('nameField');
-const resetButton = document.getElementById('reset-button');
-
-
-resetButton.addEventListener('click', clearErrorMsg);
-
-function myFunction() {
+function scrollFunction() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky")
   } else {
     navbar.classList.remove("sticky");
   }
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
 }
 
-function validateForm() {
-    var nameCheck = document.forms["contatoform"]["name"].value;
-    var mailCheck = document.forms["contatoform"]["mail"].value;
-    var commentCheck = document.forms["contatoform"]["comment"].value;
-    if (nameCheck == "" || mailCheck == "" || commentCheck == "") {
-      errorMsg.textContent = "Por favor, preencha todos os campos.";
-      return false;
-    } else {
-        errorMsg.textContent = "";
-        return true;
-    }
-  }
+function copyEmail() {
+  /* Get the text field */
+  var copyText = document.getElementById("mailCopy");
 
-function clearErrorMsg() {
-    errorMsg.textContent = "";
-    nameField.focus();
+  /* Select the text field */
+  copyText.select();
+
+  /* Copy the text inside the text field */
+  document.execCommand("copy");
+
+  /* Alert the copied text */
+  alert("Texto copiado: " + copyText.value);
+}
+
+// When the user scrolls down 20px from the top of the document, show the button
+
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
